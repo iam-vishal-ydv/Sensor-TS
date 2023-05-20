0f8b481953479cc85f7448a34146fcefbd96b27f
Varun Saxena <124664476+VarunUxdlab@users.noreply.github.com>
2023-05-20 19:06:13 +0530

    home integreted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
- import Footer from "./Components/Footer/Footer"
+ import { Route, Routes } from "react-router-dom"
+import Footer from "./Components/Footer/Footer"
 import {NavBar} from "./Components/Header/NavBar"
+import Home from "./Components/Home/Home"
 
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
   return (
     <>
      <NavBar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+     </Routes>
+     <Footer/>
     </>
   )
 }
