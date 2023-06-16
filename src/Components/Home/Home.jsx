e0f7461b086358f0a69687e04babbcc7f2cb41b4
Varun Saxena <124664476+VarunUxdlab@users.noreply.github.com>
2023-06-17 01:02:23 +0530

    map done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,8 +18,8 @@ export default function Home({setAlert}) {
           <Grid item md={12} className="px-3 text-center ">
             <h2 className=" mt-5 pt-12 text_style">
               Welcome to <br />
-              <div className=" w-[360px] h-11 m-auto">
-                <h1 className="animate-typing overflow-hidden m-auto whitespace-nowrap border-r-4 border-r-white  text-4xl font-bold text-orange-500">
+              <div className=" w-[335px] h-11 m-auto">
+                <h1 className="animate-typing overflow-hidden m-auto whitespace-nowrap border-r-4 border-r-white  text-3xl font-bold text-orange-500">
                    Cencertek Solutions
                 </h1>
               </div>
@@ -34,10 +34,22 @@ export default function Home({setAlert}) {
         </Grid>
       </div>
 
-      <div className="contact_container bg">
-        <span className="c_heading">Contact Us For More Details</span>
+      <div style={{position:'relative'}}>
+        <h1 className='about-product-heading2 text-center pb-5 text-gray-700'>Contact Us</h1>
+        </div>
+      <Grid container>
+        <Grid item md={6} xs={12} py={2} px={3}>
         <ContactFields setAlert={setAlert} />
-      </div>
+        </Grid>
+        <Grid item md={6} xs={12} p={2}>
+        <div class="mapouter">
+          <div class="gmap_canvas">
+            <iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q= Marg 155, Vikas Marg, Veer Savarkar Block, Block U, Shakarpur Khas, New Delhi, Delhi 110092&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+            </div>
+        </div>
+        </Grid>
+      </Grid>
+
     </>
   );
 }
