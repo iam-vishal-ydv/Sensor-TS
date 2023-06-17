3e22e987eb90228bac339f344d7ef40b9a244f45
iam-vishal-ydv <115427570+iam-vishal-ydv@users.noreply.github.com>
2023-06-17 12:19:10 +0530

    single page delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Home from "./Components/Home/Home";
 import About from "./Components/AboutUs/About";
 import ContactUs from "./Components/ContactUs/ContactUs";
 import RetailProjects from "./Components/Retail Projects/RetailProjects";
-import SingleProject from "./Components/Retail Projects/SingleProject";
+// import SingleProject from "./Components/Retail Projects/SingleProject";
 import SingleProduct from "./Components/Retail Projects/SingleProduct";
 import { useState } from "react";
 import ContactFields from "./Components/ContactUs/ContactFields";
+
 
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
         <Route path="/Sensor-TS/about" element={<About setAlert={setAlert}/>} />
         <Route path="/Sensor-TS/contact" element={<ContactUs setAlert={setAlert} alert={alert}/>} />
         <Route path="/Sensor-TS/retail-projects" element={<RetailProjects setAlert={setAlert}/>} />
-        <Route path="/Sensor-TS/retail-projects/singleproject/:id" element={<SingleProject setAlert={setAlert}/>} />
+   
         <Route path="/Sensor-TS/retail-projects/singleproduct/:id" element={<SingleProduct setAlert={setAlert}/>} />
         <Route path="/msg" element={<ContactFields alert={alert} />} />
       </Routes>
